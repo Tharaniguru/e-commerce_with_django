8cd62931deec365a1a7e59dfa999b0faa57e9779
Tharaniguru <125856377+Tharaniguru@users.noreply.github.com>
2024-11-06 23:11:38 +0530

    django admin

--- backend/myenv/ecomproject/ecomapp/views.py
+++ backend/myenv/ecomproject/ecomapp/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse,HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .products import products
+# from .products import products
+from .models import Products
+from .serializer import ProductSerializer
+
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -10,4 +13,7 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProduct(request):
-    return Response(products)
+    products =Products.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+    
--- backend/myenv/ecomproject/ecomproject/settings.py
+++ backend/myenv/ecomproject/ecomproject/settings.py
@@ -122,7 +122,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL='/images/'
+
+STATICFILES_DIRS=[
+    BASE_DIR /'static'
+]
+MEDIA_ROOT ='static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGIN = True
