bbc1344af6ccc07f3a9ca9d41266f1a1e2ee4ce7
Tharaniguru <125856377+Tharaniguru@users.noreply.github.com>
2024-11-08 16:16:43 +0530

    redux

--- frontend/ecom/src/components/products.js
+++ frontend/ecom/src/components/products.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Card, CardBody, CardTitle} from 'react-bootstrap'
+import {Card, CardBody,} from 'react-bootstrap'
 import Rating from './Rating'
 import { Link } from 'react-router-dom'
 
--- frontend/ecom/src/components/screen/HomeScreen.js
+++ frontend/ecom/src/components/screen/HomeScreen.js
@@ -1,30 +1,37 @@
+/* eslint-disable no-unused-vars */
 import React ,{useEffect, useState} from 'react';
 import {Container} from "react-bootstrap";
 import axios from 'axios';
 import {Row,Col,Card} from 'react-bootstrap';
 import '../../styles/HomeScreen.css'
 import Products from '../products';
+import { listProducts } from '../../actions/productActions';
+import { useDispatch,useSelector } from 'react-redux';
 
 
 function HomeScreen() {
-const[products,setProduct]=useState([])
+
+
+const dispatch =useDispatch()
+const productsList = useSelector((state)=>state.productsList);
+
+const {error , loading ,products} =productsList
 
 useEffect(()=> {
-  async function fetchproducts() {
-    const {data} = await axios.get('/api/products/')
-    setProduct(data)
-    
-  }
-  fetchproducts()
-},[])
+  dispatch(listProducts())
+},[dispatch])
 
   return (
     <Container>
       <br/>
       <h1>products</h1>
-      
-
-      <Row>
+      {
+        loading?(
+          <h2>loading...</h2>
+        ):error?(
+          <h3>{error}</h3>
+        ):(
+          <Row>
         {products.map((product)=>(
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
           
@@ -33,8 +40,12 @@ useEffect(()=> {
           </Col>
         ))}
       </Row>
+        )
+      }
+
+      
     </Container>
-  )
+  );
 }
 
 export default HomeScreen
